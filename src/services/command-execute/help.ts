8515f2344e91ccc134a32b99f76c529da985bbb5
Lui Franco Rocha <lui@prontocombustiveis.com.br>
2020-08-21 12:00:21 -0300

    feat: help command function

---
+++ src/services/command-execute/help.ts
@@ -0,0 +1,19 @@
+import { bot } from '../telegram-bot'
+
+export const helpCommand = () => {
+  const contact = process.env.DEV_CONTACT;
+  const contactText = contact ? `Qualquer duvida ou sugestão, so chamar: ${contact}` : '';
+  bot.sendMessage(
+    `Olar eu posso te mostrar informações uteis sobre as materias do seu curso!\n` +
+    `Comandos disponiveis:\n\n` +
+    ` - /help: mostra essa mensage aqui\n` +
+    ` - /salvarmaterias: depois de mandar esse comando, me envie seu resumo escolar e eu vou salvar as materias que voce ja foi aprovado, e então posso de dar dados uteis atraves de outros comandos!\n` +
+    ` - /materiasaprovadas: mostra suas materias aprovadas, para isso voce precisa usar o /salvarmaterias primeiro\n` +
+    ` - /info: mostra informações sobre uma materia, voce pode me falar o nome da materia junto com o comando ou na proxima mensagem\n` +
+    ` - /prereq: mostra os prerequisitos de uma materia, voce pode me falar o nome da materia junto com o comando ou na proxima mensagem\n` +
+    ` - /list: mostra todas as materias do seu curso, voce pode filtrar a lista de materias, mande o que voce quiser usar para filtar junto com o comando\n` +
+    ` -- Ex: /list comp\n` +
+    ` -- Mostra todas as materias que incluem comp no nome\n` +
+    `\n${contactText}`
+  );
+}
